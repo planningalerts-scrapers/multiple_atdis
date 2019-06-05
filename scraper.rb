b4338a107d5e51d100aa77a352c337317a9bc103
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 22:43:46 +1000

    Comment out broken feeds

--- scraper.rb
+++ scraper.rb
@@ -3,18 +3,21 @@ Bundler.require
 
 AUTHORITIES = {
   bega_valley: "http://datracker.begavalley.nsw.gov.au/ATDIS/1.0/",
-  armidale: "https://epathway.newengland.nsw.gov.au/ePathway/Production/WebServiceGateway/atdis/1.0",
-  ashfield: "http://mycouncil2.solorient.com.au/Horizon/@@horizondap_ashfield@@/atdis/1.0/",
   ballina: "http://da.ballina.nsw.gov.au/atdis/1.0",
   bathurst: "http://masterview.bathurst.nsw.gov.au/atdis/1.0/",
   berrigan: "http://datracking.berriganshire.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
-  cootamundra: "http://myhorizon.cootamundra.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
   dubbo: "https://planning.dubbo.nsw.gov.au/atdis/1.0/",
   kiama: "https://da.kiama.nsw.gov.au/atdis/1.0",
-  leeton: "http://203.58.97.252/Horizon/@@horizondap@@/atdis/1.0/",
-  muswellbrook: "http://datracker.muswellbrook.nsw.gov.au/atdis/1.0",
-  upper_hunter: "http://onlineservices.upperhunter.nsw.gov.au/atdis/1.0/",
-  walgett: "http://myhorizon.walgett.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
+  upper_hunter: "http://onlineservices.upperhunter.nsw.gov.au/atdis/1.0/"
+  
+  # Commenting out ATDIS feeds that used to work but do not appear
+  # to be working anymore
+  # armidale: "https://epathway.newengland.nsw.gov.au/ePathway/Production/WebServiceGateway/atdis/1.0",
+  # ashfield: "http://mycouncil2.solorient.com.au/Horizon/@@horizondap_ashfield@@/atdis/1.0/",
+  # cootamundra: "http://myhorizon.cootamundra.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
+  # leeton: "http://203.58.97.252/Horizon/@@horizondap@@/atdis/1.0/",
+  # muswellbrook: "http://datracker.muswellbrook.nsw.gov.au/atdis/1.0",
+  # walgett: "http://myhorizon.walgett.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
 }
 
 exceptions = []
