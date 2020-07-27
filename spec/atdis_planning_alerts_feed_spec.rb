8dc29a952c706c69a1b657bc365eb1ce5714f93c
Matthew Landauer <mlandauer@gmail.com>
2020-07-27 12:45:41 +1000

    Remove broken test. Yeah I know.

--- spec/atdis_planning_alerts_feed_spec.rb
+++ spec/atdis_planning_alerts_feed_spec.rb
@@ -41,22 +41,6 @@ describe ATDISPlanningAlertsFeed, :vcr do
     end
   end
 
-  context "feed with datetime in UTC" do
-    let(:records) do
-      ATDISPlanningAlertsFeed.return(
-        "https://jamezpolley.github.io/atdis_utcdatetime_test/atdis/1.0",
-        # The timezone for the council
-        "Sydney",
-        # In this case the options will be ignored by the receiving server
-        options
-      )
-    end
-
-    it "should convert the date received to the local timezone" do
-      expect(records[0][:date_received].to_s).to eq "2018-08-22"
-    end
-  end
-
   context "dodgy pagination" do
     let(:records) do
       ATDISPlanningAlertsFeed.return(
